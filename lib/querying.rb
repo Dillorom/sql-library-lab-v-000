8ee082977dfac4f268972ba68cd41af4e89d2556
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-20 18:55:54 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,9 +5,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters
+  "SELECT characters.name, characters.motto, (length(characters.motto) FROM characters
   GROUP BY characters.name
-  ORDER BY COUNT(length(characters.motto)) ASC LIMIT 1"
+  ORDER BY COUNT) ASC LIMIT 1"
 end
 
 
